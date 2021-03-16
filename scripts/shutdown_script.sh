89bbfc7cd5433e00ce199b29afb728b3f54038dd
sunilRF9 <sunil1996@gmail.com>
2021-03-16 11:37:09 +0530

    added VSCODE settings.json

--- scripts/shutdown_script.sh
+++
@@ -1,5 +0,0 @@
-[ $(echo -e "Beda\nMaadi" | dmenu -i -p "off maadbeka??") \
-== "Maadi" ] && $(shutdown -h now)
-#RED
-#[[ $(echo -e "Beda\nMaadi" | dmenu -nb 'red' -sf 'white' -sb 'black' -nf 'black' -fn 'Fira Code-10' -i -p "Off maadbeka?") \
-#== "Maadi" ]] && $(shutdown -h now)
--- status.sh
+++ status.sh
@@ -3,18 +3,7 @@ while true
 do
 DATE=`date | cut -c 1-16`
 TIME=`date +"%T"`
-#DATE=`date +%D`
-UPTIME=`uptime | sed 's/.*up\s*//' | sed 's/,\s*[0-9]* user.*//' | sed 's/  / /g'`
 BATTERYSTATE=$( acpi -b | awk '{ split($5,a,":"); print substr($3,0,2), $4, "["a[1]":"a[2]"]" }' | tr -d ',' )
-ssid=$(iwgetid -r)
-#cpu=$(sensors | awk '/Core 0/ {print "🌡", $3}')
-int=$(internet)
-#ram=$(memory)
-#back=$(backlight)
-#vol=$(volume)·
-#green=$(tput setaf 2; ip route get 1.2.3.4 | awk '{print $7}')
-#ip=$(ip route get 1.2.3.4 | awk '{print $7}')
-#ipsx=$(ip -6 addr|awk '{print $2}'|grep -P '^(?!fe80)[[:alnum:]]{4}:.*/64'|cut -d '/' -f1)
-xsetroot -name " ${int} · ${ssid} · $(df -Ph | grep "/dev/sda3" | awk {'print $4'}) · $(battery) · ${DATE}${TIME}"
+xsetroot -name "$(df -Ph | grep "/dev/sda3" | awk {'print $4'}) · ${BATTERYSTATE} · ${DATE}${TIME}"
 sleep 5s
 done &
